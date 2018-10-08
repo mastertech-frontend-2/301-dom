880050231c5ee1689aaf846de1ea4a282b2dafb9
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-10-08 20:28:28 -0300

    Adicionar exemplos de metodos de nos do DOM

--- script.js
+++ script.js
@@ -3,3 +3,12 @@
 console.log(document); //Document contém toda a estrutura HTML da sua página
 
 const divCentral = document.querySelector('#central');
+
+//.tagName retorna o nome da Tag do objeto que você está trabalhando, em caixa alta
+console.log(divCentral.tagName);
+
+//.id retorna o nome da Id que o objeto que você está trabalhando tem, ou uma string vazia caso não haja Id
+console.log(divCentral.id);
+
+//.innerHTML retorna todo o conteúdo que existe entre a abertura e o fechamento de uma tag, ou uma string vazia caso não haja conteúdo
+console.log(divCentral.innerHTML);
