e5d91f25e9c65fb41c7a2b0587e9cfa703194e18
Felipe Teixeira Moraes <felipetm6@hotmail.com>
2018-10-08 19:48:08 -0300

    Adicionar exemplos de Arrow Function

---
+++ arrow.js
@@ -0,0 +1,23 @@
+// function f1(a) {
+//   return a+1;
+// }
+
+// function f2(a, b, c) {
+//   return (a+b+c);
+// }
+
+// function contraFatos() {
+//   console.log('abcd');
+//   console.log('xpto');
+//   return false;
+// }
+
+const f1 = a => a + 1;
+
+const f2 = (a, b, c) => a+b+c;
+
+const contraFatos = () => {
+  console.log('abcd');
+  console.log('xpto');
+  return false;
+}
